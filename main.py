9edcf9ae34829e325de1690a56dc90b05382261c
Lasha <50033103+MistbornL@users.noreply.github.com>
2021-11-05 18:59:03 +0400

    Update main.py

--- main.py
+++ main.py
@@ -6,34 +6,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# origins = [
-#     "http://localhost:3000",
-#     "localhost:3000"
-# ]
-
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"]
-# )
-
-# @app.websocket("/ws")
-# async def websocket_endpoint(websocket:WebSocket):
-#     print("Accept connection")
-#     await websocket.accept()
-#     print("accepted")
-#     while True:
-#         try:
-#             data = await websocket.receive_text()
-#             print(data)
-#         except:
-#             pass
-
-
-
 
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
 app = socketio.ASGIApp(sio)
